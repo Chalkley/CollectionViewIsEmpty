a616bcf02faaf2cc40c41aa4a502385c668ba01f
Jason Chalkley <jaychalks@gmail.com>
2018-12-14 20:46:29 +0000

    Initial Commit

---
+++ CollectionViewIsEmpty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewIsEmpty
+//
+//  Created by Jason Chalkley on 14/12/2018.
+//  Copyright © 2018 Jason Chalkley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
